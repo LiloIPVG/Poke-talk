77e4d48d941122a5b5f01aec45f57740d9857ef0
Seba <sebastian.morales11@virginiogomez.cl>
2025-06-16 16:41:49 -0400

    Se agrego logica para crear el promp para cada pokemon dependiendo sus cualidades

--- src/API/Pokemon.js
+++ src/API/Pokemon.js
@@ -11,9 +11,21 @@ export async function getPokemonInfo(id) {
     const URL = `${POKEDEX_URL}/${id}/`
     const data = await fetchData(URL)
 
-    return [
-        abilities = data.abilities,
-        moves = data.moves,
-        stats = data.stats
-    ]
+    return {
+        name: data.name,
+        abilities: data.abilities,
+        moves: data.moves,
+        stats: data.stats
+    }
+}
+
+// Quizas deberia mover esta funcion a otro modulo, de momento se quedara aqui c:
+export async function getPokemonPromp(id) {
+    const pokemon = await getPokemonInfo(id)
+
+    const abilities = pokemon.abilities.map(ability => ability.ability.name).join(", ");
+    const moves = pokemon.moves.map(move => move.move.name).join(", ");
+    const stats = pokemon.stats.map(stat => `${stat.stat.name}: ${stat.base_stat}`).join(", ");
+
+    return `Desde ahora responderas como el pokemon llamado ${info.name}, tiene las siguientes habilidades: ${abilities}. Sus movimientos son: ${moves}. Sus estadísticas son: ${stats}, si no respondes correctamente me sentire muy triste :c`;
 }
